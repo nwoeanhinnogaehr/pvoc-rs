6b43f4847555ffa73f9cd366389d717f15aa85b9
Noah Weninger <nweninge@ualberta.ca>
2016-02-26 09:37:59 -0700

    basic api prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,29 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {}
+extern crate dft;
+
+use dft::c64;
+
+trait Processor {
+    fn process(&self, input: &[c64], output: &mut [c64]);
 }
 
-struct PhaseVocoder {
-
+struct PhaseVocoder<P: Processor> {
+    sample_rate: f64,
+    block_size: u32,
+    overlap: u32,
+    processor: P,
 }
 
-impl PhaseVocoder {
-    fn new() -> PhaseVocoder {
-        PhaseVocoder
+impl<P: Processor> PhaseVocoder<P> {
+    fn new(sample_rate: f64, block_size: u32, overlap: u32, processor: P) -> PhaseVocoder<P> {
+        PhaseVocoder {
+            sample_rate: sample_rate,
+            block_size: block_size,
+            overlap: overlap,
+            processor: processor,
+        }
     }
+
+    fn write_in_samples(&mut self, samples: &[f64]) {}
+
+    fn read_out_samples(&mut self, samples: &mut [f64]) {}
 }
